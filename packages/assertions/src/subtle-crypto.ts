cd0755909f803b6117d115e50a86c7a7103d8aac
Steven Luscher <steveluscher@users.noreply.github.com>
2025-01-20 01:29:44 -0800

    Document `@solana/assertions` with TypeDoc (#64)

--- packages/assertions/src/subtle-crypto.ts
+++ packages/assertions/src/subtle-crypto.ts
@@ -36,6 +36,10 @@ async function isEd25519CurveSupported(subtle: SubtleCrypto): Promise<boolean> {
     }
 }
 
+/**
+ * Throws an exception unless {@link SubtleCrypto#digest | `crypto.subtle.digest()`} is available in
+ * the current JavaScript environment.
+ */
 export function assertDigestCapabilityIsAvailable() {
     assertIsSecureContext();
     if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.digest !== 'function') {
@@ -43,6 +47,10 @@ export function assertDigestCapabilityIsAvailable() {
     }
 }
 
+/**
+ * Throws an exception unless {@link SubtleCrypto#generateKey | `crypto.subtle.generateKey()`} is
+ * available in the current JavaScript environment and has support for the Ed25519 curve.
+ */
 export async function assertKeyGenerationIsAvailable() {
     assertIsSecureContext();
     if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.generateKey !== 'function') {
@@ -53,6 +61,10 @@ export async function assertKeyGenerationIsAvailable() {
     }
 }
 
+/**
+ * Throws an exception unless {@link SubtleCrypto#exportKey | `crypto.subtle.exportKey()`} is
+ * available in the current JavaScript environment.
+ */
 export function assertKeyExporterIsAvailable() {
     assertIsSecureContext();
     if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.exportKey !== 'function') {
@@ -60,13 +72,20 @@ export function assertKeyExporterIsAvailable() {
     }
 }
 
+/**
+ * Throws an exception unless {@link SubtleCrypto#sign | `crypto.subtle.sign()`} is available in the
+ * current JavaScript environment.
+ */
 export function assertSigningCapabilityIsAvailable() {
     assertIsSecureContext();
     if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.sign !== 'function') {
         throw new SolanaError(SOLANA_ERROR__SUBTLE_CRYPTO__SIGN_FUNCTION_UNIMPLEMENTED);
     }
 }
-
+/**
+ * Throws an exception unless {@link SubtleCrypto#verify | `crypto.subtle.verify()`} is available in
+ * the current JavaScript environment.
+ */
 export function assertVerificationCapabilityIsAvailable() {
     assertIsSecureContext();
     if (typeof globalThis.crypto === 'undefined' || typeof globalThis.crypto.subtle?.verify !== 'function') {
