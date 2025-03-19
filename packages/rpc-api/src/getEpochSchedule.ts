4db2150729686ab98adf4460fa5f70ce3b546619
Steven Luscher <steveluscher@users.noreply.github.com>
2025-03-19 16:21:23 -0700

    Document the `getEpochSchedule` method of the RPC API (#239)

--- packages/rpc-api/src/getEpochSchedule.ts
+++ packages/rpc-api/src/getEpochSchedule.ts
@@ -1,19 +1,24 @@
 type GetEpochScheduleApiResponse = Readonly<{
-    /** first normal-length epoch, log2(slotsPerEpoch) - log2(MINIMUM_SLOTS_PER_EPOCH) */
+    /** First normal-length epoch, log2(slotsPerEpoch) - log2(MINIMUM_SLOTS_PER_EPOCH) */
     firstNormalEpoch: bigint;
     /** MINIMUM_SLOTS_PER_EPOCH * (2^(firstNormalEpoch) - 1) */
     firstNormalSlot: bigint;
-    /** the number of slots before beginning of an epoch to calculate a leader schedule for that epoch */
+    /**
+     * The number of slots before beginning of an epoch to calculate a leader schedule for that
+     * epoch
+     */
     leaderScheduleSlotOffset: bigint;
-    /** the maximum number of slots in each epoch */
+    /** The maximum number of slots in each epoch */
     slotsPerEpoch: bigint;
-    /** whether epochs start short and grow */
+    /** Whether epochs start short and grow */
     warmup: boolean;
 }>;
 
 export type GetEpochScheduleApi = {
     /**
      * Returns the epoch schedule information from this cluster's genesis config
+     *
+     * @see https://solana.com/docs/rpc/http/getepochschedule
      */
     getEpochSchedule(): GetEpochScheduleApiResponse;
 };
