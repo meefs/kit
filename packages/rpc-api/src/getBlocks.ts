59d919fe6efa7ab1b82394beaa20e3a2b2c3bd6c
Steven Luscher <steveluscher@users.noreply.github.com>
2025-03-19 15:00:23 -0700

    Document `getBlocks` method of the RPC API (#233)

--- packages/rpc-api/src/getBlocks.ts
+++ packages/rpc-api/src/getBlocks.ts
@@ -4,10 +4,20 @@ type GetBlocksApiResponse = Slot[];
 
 export type GetBlocksApi = {
     /**
-     * Returns a list of confirmed blocks between two slots
+     * Returns a list of confirmed blocks between two slots (inclusive).
+     *
+     * @see https://solana.com/docs/rpc/http/getblocks
      */
     getBlocks(
+        /** The first slot for which to return a confirmed block */
         startSlot: Slot,
+        /**
+         * The last slot for which to return a confirmed block.
+         *
+         * Must be no more than 500,000 blocks higher than the start slot.
+         *
+         * @defaultValue If not provided, defaults to the latest confirmed slot.
+         */
         endSlotInclusive?: Slot,
         config?: Readonly<{
             /**
