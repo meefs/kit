89c4726a5c552438fc11f67761b6b46232604ba4
Steven Luscher <steveluscher@users.noreply.github.com>
2025-03-21 13:59:44 -0700

    Document the `getProgramAccounts()` method of the RPC API (#266)

--- packages/rpc-api/src/getProgramAccounts.ts
+++ packages/rpc-api/src/getProgramAccounts.ts
@@ -26,13 +26,43 @@ type GetProgramAccountsApiCommonConfig = Readonly<{
      * default commitment applied by the server is `"finalized"`.
      */
     commitment?: Commitment;
-    /** filter results (up to 4 filters allowed) @see https://docs.solana.com/api/http#filter-criteria */
+    /**
+     * Determines how the accounts' data should be encoded in the response.
+     *
+     * - `'base58'` returns a tuple whose first element is the data as a base58-encoded string. If
+     *   the account contains more than 129 bytes of data, an error will be raised.
+     * - `'base64'` returns a tuple whose first element is the data as a base64-encoded string.
+     * - `'base64+zstd'` returns a tuple whose first element is the
+     *   [ZStandard](https://facebook.github.io/zstd/)-compressed data as a base64-encoded string.
+     * - `'jsonParsed'` will cause the server to attempt to process the data using a parser specific
+     *   to the owning program. If successful, the parsed data will be returned in the response as
+     *   JSON. Otherwise, the raw account data will be returned in the response as a tuple whose
+     *   first element is a base64-encoded string.
+     */
+    encoding?: 'base58' | 'base64' | 'base64+zstd' | 'jsonParsed';
+    /**
+     * Limits results to those that match all of these filters.
+     *
+     * This is useful when your aim is to find program accounts whose purpose is uniquely determined
+     * by having a data buffer of a known size, or whose data contains a known series of bytes (eg.
+     * a discriminator).
+     *
+     * You can specify up to 4 filters.
+     *
+     * @defaultValue When omitted, no filters are applied.
+     */
     filters?: (GetProgramAccountsDatasizeFilter | GetProgramAccountsMemcmpFilter)[];
     /**
      * Prevents accessing stale data by enforcing that the RPC node has processed transactions up to
      * this slot
      */
     minContextSlot?: Slot;
+    /**
+     * Wraps the result in an {@link RpcResponse} when `true`
+     *
+     * @defaultValue false
+     */
+    withContext?: boolean;
 }>;
 
 type GetProgramAccountsApiSliceableCommonConfig = Readonly<{
@@ -45,9 +75,16 @@ type GetProgramAccountsApiSliceableCommonConfig = Readonly<{
      */
     dataSlice?: DataSlice;
 }>;
+
 export type GetProgramAccountsApi = {
     /**
-     * Returns the account information for a list of Pubkeys.
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, it will be returned in the response as a tuple whose first element
+     * is a base64-encoded string.
+     *
+     * {@label base64-withcontext}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
      */
     getProgramAccounts(
         program: Address,
@@ -58,7 +95,15 @@ export type GetProgramAccountsApi = {
                 withContext: true;
             }>,
     ): SolanaRpcResponse<AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithBase64EncodedData>[]>;
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, it will be returned in the response as a tuple whose first element
+     * is a base64-encoded string.
+     *
+     * {@label base64}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config: GetProgramAccountsApiCommonConfig &
@@ -68,7 +113,16 @@ export type GetProgramAccountsApi = {
                 withContext?: boolean;
             }>,
     ): AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithBase64EncodedData>[];
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, it will first be compressed using
+     * [ZStandard](https://facebook.github.io/zstd/) and the result will be returned in the response
+     * as a tuple whose first element is a base64-encoded string.
+     *
+     * {@label base64-zstd-compressed-withcontext}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config: GetProgramAccountsApiCommonConfig &
@@ -78,7 +132,16 @@ export type GetProgramAccountsApi = {
                 withContext: true;
             }>,
     ): SolanaRpcResponse<AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithBase64EncodedZStdCompressedData>[]>;
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, it will first be compressed using
+     * [ZStandard](https://facebook.github.io/zstd/) and the result will be returned in the response
+     * as a tuple whose first element is a base64-encoded string.
+     *
+     * {@label base64-zstd-compressed}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config: GetProgramAccountsApiCommonConfig &
@@ -88,7 +151,17 @@ export type GetProgramAccountsApi = {
                 withContext?: boolean;
             }>,
     ): AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithBase64EncodedZStdCompressedData>[];
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, the server will attempt to process it using a parser specific to
+     * the owning program. If successful, the parsed data will be returned in the response as JSON.
+     * Otherwise, the raw account data will be returned in the response as a tuple whose first
+     * element is a base64-encoded string.
+     *
+     * {@label parsed-withcontext}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config: GetProgramAccountsApiCommonConfig &
@@ -97,7 +170,17 @@ export type GetProgramAccountsApi = {
                 withContext: true;
             }>,
     ): SolanaRpcResponse<AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithJsonData>[]>;
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, the server will attempt to process it using a parser specific to
+     * the owning program. If successful, the parsed data will be returned in the response as JSON.
+     * Otherwise, the raw account data will be returned in the response as a tuple whose first
+     * element is a base64-encoded string.
+     *
+     * {@label parsed}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config: GetProgramAccountsApiCommonConfig &
@@ -106,7 +189,16 @@ export type GetProgramAccountsApi = {
                 withContext?: boolean;
             }>,
     ): AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithJsonData>[];
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, it will be returned in the response as a tuple whose first element
+     * is a base58-encoded string. If any account contains more than 129 bytes of data, this method
+     * will raise an error.
+     *
+     * {@label base58-withcontext}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config: GetProgramAccountsApiCommonConfig &
@@ -116,7 +208,16 @@ export type GetProgramAccountsApi = {
                 withContext: true;
             }>,
     ): SolanaRpcResponse<AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithBase58EncodedData>[]>;
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, it will be returned in the response as a tuple whose first element
+     * is a base58-encoded string. If any account contains more than 129 bytes of data, this method
+     * will raise an error.
+     *
+     * {@label base58}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config: GetProgramAccountsApiCommonConfig &
@@ -126,7 +227,15 @@ export type GetProgramAccountsApi = {
                 withContext?: boolean;
             }>,
     ): AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithBase58EncodedData>[];
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, it will be returned in the response as a base58-encoded string. If
+     * any account contains more than 129 bytes of data, this method will raise an error.
+     *
+     * {@label base58-legacy-withcontext}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config: GetProgramAccountsApiCommonConfig &
@@ -135,7 +244,15 @@ export type GetProgramAccountsApi = {
                 withContext: true;
             }>,
     ): SolanaRpcResponse<AccountInfoWithPubkey<AccountInfoBase & AccountInfoWithBase58Bytes>[]>;
-
+    /**
+     * Fetches information associated with all accounts owned by the program at the given address.
+     *
+     * If the accounts have data, it will be returned in the response as a base58-encoded string. If
+     * any account contains more than 129 bytes of data, this method will raise an error.
+     *
+     * {@label base58-legacy-withcontext}
+     * @see https://solana.com/docs/rpc/http/getprogramaccounts
+     */
     getProgramAccounts(
         program: Address,
         config?: GetProgramAccountsApiCommonConfig &
--- packages/rpc-types/src/account-filters.ts
+++ packages/rpc-types/src/account-filters.ts
@@ -6,13 +6,24 @@ export type DataSlice = Readonly<{
 }>;
 
 export type GetProgramAccountsMemcmpFilter = Readonly<{
+    /**
+     * This filter matches when the bytes supplied are equal to the account data at the given offset
+     */
     memcmp: Readonly<{
+        /**
+         * The bytes to match, encoded as a string using the specified encoding.
+         *
+         * Data is limited to a maximum of 128 decoded bytes.
+         */
         bytes: string;
+        /** The encoding to use when decoding the supplied byte string */
         encoding: 'base58' | 'base64';
+        /** The byte offset into the account data from which to start the comparison */
         offset: bigint;
     }>;
 }>;
 
 export type GetProgramAccountsDatasizeFilter = Readonly<{
+    /** This filter matches when the account data length is equal to this */
     dataSize: bigint;
 }>;
