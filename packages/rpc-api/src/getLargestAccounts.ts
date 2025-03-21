7ef9f77e9d0a4543ad0fedbd86019f94aef7b8a1
Steven Luscher <steveluscher@users.noreply.github.com>
2025-03-21 12:21:25 -0700

    Document the `getLargestAccounts()` method of the RPC API (#255)

--- packages/rpc-api/src/getLargestAccounts.ts
+++ packages/rpc-api/src/getLargestAccounts.ts
@@ -4,7 +4,7 @@ import type { Commitment, Lamports, SolanaRpcResponse } from '@solana/rpc-types'
 type GetLargestAccountsResponseItem = Readonly<{
     /** Base-58 encoded address of the account */
     address: Address;
-    /** Number of lamports in the account */
+    /** Number of {@link Lamports} in the account */
     lamports: Lamports;
 }>;
 
@@ -12,8 +12,11 @@ type GetLargestAccountsApiResponse = readonly GetLargestAccountsResponseItem[];
 
 export type GetLargestAccountsApi = {
     /**
-     * Returns the 20 largest accounts, by lamport balance
-     * (results may be cached up to two hours)
+     * Returns the 20 largest accounts, by {@link Lamports | Lamport} balance.
+     *
+     * Results may be cached up to two hours.
+     *
+     * @see https://solana.com/docs/rpc/http/getlargestaccounts
      */
     getLargestAccounts(
         config?: Readonly<{
@@ -27,7 +30,12 @@ export type GetLargestAccountsApi = {
              * on the client, the default commitment applied by the server is `"finalized"`.
              */
             commitment?: Commitment;
-            /** Filter results by account type */
+            /**
+             * Filter results by account type.
+             *
+             * @defaultValue When not specified, all accounts will be considered regardless of their
+             * type.
+             */
             filter?: 'circulating' | 'nonCirculating';
         }>,
     ): SolanaRpcResponse<GetLargestAccountsApiResponse>;
