316af7d2c9008030401d0349ee05786ae96799a9
Steven Luscher <steveluscher@users.noreply.github.com>
2025-03-25 06:52:34 -0700

    Document the `getLeaderSchedule()` method of the RPC API (#257)

--- packages/rpc-api/src/__typetests__/get-leader-schedule-type-test.ts
+++ packages/rpc-api/src/__typetests__/get-leader-schedule-type-test.ts
@@ -10,15 +10,21 @@ const identity = 'Joe11111111111111111111111111111' as Address<'Joe1111111111111
 
 void (async () => {
     {
-        const result = await rpc.getLeaderSchedule(slot).send();
-        // Can be null if the slot corresponds to an epoch that does not exist
-        result satisfies Record<Address, Slot[]> | null;
+        const result = await rpc.getLeaderSchedule().send();
+        // Won't be null
+        result satisfies Record<Address, Slot[]>;
     }
 
     {
         const result = await rpc.getLeaderSchedule(null).send();
         // Won't be null
         result satisfies Record<Address, Slot[]>;
+    }
+
+    {
+        const result = await rpc.getLeaderSchedule(slot).send();
+        // Can be null if the slot corresponds to an epoch that does not exist
+        result satisfies Record<Address, Slot[]> | null;
     }
 
     {
--- packages/rpc-api/src/getLeaderSchedule.ts
+++ packages/rpc-api/src/getLeaderSchedule.ts
@@ -13,22 +13,20 @@ type GetLeaderScheduleApiConfigBase = Readonly<{
     commitment?: Commitment;
 }>;
 
-/**
- * This return type is a dictionary of validator identities, as base-58 encoded
- * strings, and their corresponding leader slot indices as values
- * (indices are relative to the first slot in the requested epoch)
- * @example
- * ```json
- * {
- *   "4Qkev8aNZcqFNSRhQzwyLMFSsi94jHqE8WNVTJzTP99F": [
- *     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
- *     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
- *     39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
- *     57, 58, 59, 60, 61, 62, 63
- *   ]
- * }
- * ```
- */
+// A dictionary of validator identities as base-58 encoded strings, and their corresponding leader
+// slot indices as values relative to the first slot in the requested epoch.
+//
+// @example
+// ```json
+// {
+//   "4Qkev8aNZcqFNSRhQzwyLMFSsi94jHqE8WNVTJzTP99F": [
+//     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+//     21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38,
+//     39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56,
+//     57, 58, 59, 60, 61, 62, 63
+//   ]
+// }
+// ```
 type GetLeaderScheduleApiResponseWithAllIdentities = Record<Address, Slot[]>;
 
 type GetLeaderScheduleApiResponseWithSingleIdentity<TIdentity extends string> = Readonly<{
@@ -37,15 +35,15 @@ type GetLeaderScheduleApiResponseWithSingleIdentity<TIdentity extends string> = 
 
 export type GetLeaderScheduleApi = {
     /**
-     * Fetch the leader schedule for the epoch that corresponds to the provided slot.
-     * If unspecified, the leader schedule for the current epoch is fetched
+     * Fetch the leader schedule of a particular validator.
      *
-     * When a slot is provided, the leader schedule for the epoch that corresponds
-     * to the provided slot is returned, and this can be null if the slot corresponds
-     * to an epoch that does not exist.
+     * @param slot A slot that will be used to select the epoch for which to return the leader
+     * schedule.
      *
-     * The RPC request payload provides a `null` value for `slot` when the slot is not
-     * specified but the request wishes to include config.
+     * @returns A dictionary having a single key representing the specified validator identity, and
+     * its corresponding leader slot indices as values relative to the first slot in the requested
+     * epoch, or `null` if there is no epoch that corresponds to the given slot.
+     * @see https://solana.com/docs/rpc/http/getleaderschedule
      */
     getLeaderSchedule<TIdentity extends Address>(
         slot: Slot,
@@ -55,10 +53,31 @@ export type GetLeaderScheduleApi = {
                 identity: Address;
             }>,
     ): GetLeaderScheduleApiResponseWithSingleIdentity<TIdentity> | null;
+    /**
+     * Fetch the leader schedule for all validators.
+     *
+     * @param slot A slot that will be used to select the epoch for which to return the leader
+     * schedule.
+     *
+     * @returns A dictionary of validator identities as base-58 encoded strings, and their
+     * corresponding leader slot indices as values relative to the first slot in the requested
+     * epoch, or `null` if there is no epoch that corresponds to the given slot.
+     * @see https://solana.com/docs/rpc/http/getleaderschedule
+     */
     getLeaderSchedule(
         slot: Slot,
         config?: GetLeaderScheduleApiConfigBase,
     ): GetLeaderScheduleApiResponseWithAllIdentities | null;
+    /**
+     * Fetch the leader schedule of a particular validator.
+     *
+     * @param slot When `null`, orders the leader schedule for the current epoch.
+     *
+     * @returns A dictionary having a single key representing the specified validator identity, and
+     * its corresponding leader slot indices as values relative to the first slot in the current
+     * epoch.
+     * @see https://solana.com/docs/rpc/http/getleaderschedule
+     */
     getLeaderSchedule<TIdentity extends Address>(
         slot: null,
         config: GetLeaderScheduleApiConfigBase &
@@ -67,8 +86,18 @@ export type GetLeaderScheduleApi = {
                 identity: Address;
             }>,
     ): GetLeaderScheduleApiResponseWithSingleIdentity<TIdentity>;
+    /**
+     * Fetch the leader schedule of all validators.
+     *
+     * @param slot When `null`, orders the leader schedule for the current epoch.
+     *
+     * @returns A dictionary of validator identities as base-58 encoded strings, and their
+     * corresponding leader slot indices as values relative to the first slot in the current
+     * epoch.
+     * @see https://solana.com/docs/rpc/http/getleaderschedule
+     */
     getLeaderSchedule(
-        slot: null,
+        slot?: null,
         config?: GetLeaderScheduleApiConfigBase,
     ): GetLeaderScheduleApiResponseWithAllIdentities;
 };
