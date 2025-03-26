3271e364d45453871d87010128111b6d6d77da93
Steven Luscher <steveluscher@users.noreply.github.com>
2025-03-26 15:12:35 -0700

    Document `@solana/rpc-spec-types` with TypeDoc (#301)
    Preview at https://imaginative-manatee-c32d89.netlify.app/

--- packages/rpc-spec-types/src/rpc-message.ts
+++ packages/rpc-spec-types/src/rpc-message.ts
@@ -7,6 +7,11 @@ function getNextMessageId(): string {
     return id.toString();
 }
 
+/**
+ * Returns a spec-compliant JSON RPC 2.0 message, given a method name and some params.
+ *
+ * Generates a new `id` on each call by incrementing a `bigint` and casting it to a string.
+ */
 export function createRpcMessage<TParams>(request: RpcRequest<TParams>) {
     return {
         id: getNextMessageId(),
