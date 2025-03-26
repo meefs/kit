06e76201315abfb302a177e6794d0d394a058674
Steven Luscher <steveluscher@users.noreply.github.com>
2025-03-26 15:16:24 -0700

    Document `@solana/rpc-spec` with TypeDoc (#300)
    Preview at https://flourishing-cendol-ed0786.netlify.app/

--- packages/rpc-spec/src/rpc.ts
+++ packages/rpc-spec/src/rpc.ts
@@ -9,15 +9,33 @@ export type RpcConfig<TRpcMethods, TRpcTransport extends RpcTransport> = Readonl
     transport: TRpcTransport;
 }>;
 
+/**
+ * An object that exposes all of the functions described by `TRpcMethods`.
+ *
+ * Calling each method returns a {@link PendingRpcRequest | PendingRpcRequest<TResponse>} where
+ * `TResponse` is that method's response type.
+ */
 export type Rpc<TRpcMethods> = {
     [TMethodName in keyof TRpcMethods]: PendingRpcRequestBuilder<OverloadImplementations<TRpcMethods, TMethodName>>;
 };
 
+/**
+ * Pending requests are the result of calling a supported method on a {@link Rpc} object. They
+ * encapsulate all of the information necessary to make the request without actually making it.
+ *
+ * Calling the {@link PendingRpcRequest.send | `send(options)`} method on a
+ * {@link PendingRpcRequest | PendingRpcRequest<TResponse>} will trigger the request and return a
+ * promise for `TResponse`.
+ */
 export type PendingRpcRequest<TResponse> = {
     send(options?: RpcSendOptions): Promise<TResponse>;
 };
 
 export type RpcSendOptions = Readonly<{
+    /**
+     * An optional signal that you can supply when triggering a {@link PendingRpcRequest} that you
+     * might later need to abort.
+     */
     abortSignal?: AbortSignal;
 }>;
 
@@ -33,6 +51,10 @@ type PendingRpcRequestReturnTypeMapper<TMethodImplementation> =
         ? (...args: Parameters<TMethodImplementation>) => PendingRpcRequest<ReturnType<TMethodImplementation>>
         : never;
 
+/**
+ * Creates a {@link Rpc} instance given an {@link RpcApi | RpcApi<TRpcMethods>} and a
+ * {@link RpcTransport} capable of fulfilling them.
+ */
 export function createRpc<TRpcMethods, TRpcTransport extends RpcTransport>(
     rpcConfig: RpcConfig<TRpcMethods, TRpcTransport>,
 ): Rpc<TRpcMethods> {
