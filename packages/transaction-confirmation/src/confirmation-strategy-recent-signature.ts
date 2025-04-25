89f9ae369a5fe1bdb33318894480d8471064e1b5
Steven Luscher <steveluscher@users.noreply.github.com>
2025-04-25 13:46:02 -0700

    Document `@solana/transaction-confirmation` with TypeDoc (#380)
    * Document `@solana/transaction-confirmation` with TypeDoc

--- packages/transaction-confirmation/src/confirmation-strategy-recent-signature.ts
+++ packages/transaction-confirmation/src/confirmation-strategy-recent-signature.ts
@@ -8,7 +8,15 @@ import { type Commitment, commitmentComparator } from '@solana/rpc-types';
 
 type GetRecentSignatureConfirmationPromiseFn = (config: {
     abortSignal: AbortSignal;
+    /**
+     * The level of commitment the transaction must have achieved in order for the promise to
+     * resolve.
+     */
     commitment: Commitment;
+    /**
+     * A 64 byte Ed25519 signature, encoded as a base-58 string, that uniquely identifies a
+     * transaction by virtue of being the first or only signature in its list of signatures.
+     */
     signature: Signature;
 }) => Promise<void>;
 
@@ -17,6 +25,38 @@ type CreateRecentSignatureConfirmationPromiseFactoryConfig<TCluster> = {
     rpcSubscriptions: RpcSubscriptions<SignatureNotificationsApi> & { '~cluster'?: TCluster };
 };
 
+/**
+ * Creates a promise that resolves when a recently-landed transaction achieves the target
+ * confirmation commitment, and throws when the transaction fails with an error.
+ *
+ * The status of recently-landed transactions is available in the network's status cache. This
+ * confirmation strategy will only yield a result if the signature is still in the status cache. To
+ * fetch the status of transactions older than those available in the status cache, use the
+ * {@link GetSignatureStatusesApi.getSignatureStatuses} method setting the
+ * `searchTransactionHistory` configuration param to `true`.
+ *
+ * @param config
+ *
+ * @example
+ * ```ts
+ * import { createRecentSignatureConfirmationPromiseFactory } from '@solana/transaction-confirmation';
+ *
+ * const getRecentSignatureConfirmationPromise = createRecentSignatureConfirmationPromiseFactory({
+ *     rpc,
+ *     rpcSubscriptions,
+ * });
+ * try {
+ *     await getRecentSignatureConfirmationPromise({
+ *         commitment,
+ *         signature,
+ *     });
+ *     console.log(`The transaction with signature \`${signature}\` has achieved a commitment level of \`${commitment}\``);
+ * } catch (e) {
+ *     console.error(`The transaction with signature \`${signature}\` failed`, e.cause);
+ *     throw e;
+ * }
+ * ```
+ */
 export function createRecentSignatureConfirmationPromiseFactory({
     rpc,
     rpcSubscriptions,
--- packages/transaction-confirmation/src/waiters.ts
+++ packages/transaction-confirmation/src/waiters.ts
@@ -29,9 +29,35 @@ interface WaitForRecentTransactionWithBlockhashLifetimeConfirmationConfig
 interface WaitForRecentTransactionWithTimeBasedLifetimeConfirmationConfig
     extends BaseTransactionConfirmationStrategyConfig {
     getTimeoutPromise: typeof getTimeoutPromise;
+    /**
+     * A 64 byte Ed25519 signature, encoded as a base-58 string, that uniquely identifies a
+     * transaction by virtue of being the first or only signature in its list of signatures.
+     */
     signature: Signature;
 }
 
+/**
+ * Supply your own confirmation implementations to this function to create a custom nonce
+ * transaction confirmation strategy.
+ *
+ * @example
+ * ```ts
+ * import { waitForDurableNonceTransactionConfirmation } from '@solana/transaction-confirmation';
+ *
+ * try {
+ *     await waitForDurableNonceTransactionConfirmation({
+ *         getNonceInvalidationPromise({ abortSignal, commitment, currentNonceValue, nonceAccountAddress }) {
+ *             // Return a promise that rejects when a nonce becomes invalid.
+ *         },
+ *         getRecentSignatureConfirmationPromise({ abortSignal, commitment, signature }) {
+ *             // Return a promise that resolves when a transaction achieves confirmation
+ *         },
+ *     });
+ * } catch (e) {
+ *     // Handle errors.
+ * }
+ * ```
+ */
 export async function waitForDurableNonceTransactionConfirmation(
     config: WaitForDurableNonceTransactionConfirmationConfig,
 ): Promise<void> {
@@ -51,6 +77,28 @@ export async function waitForDurableNonceTransactionConfirmation(
     );
 }
 
+/**
+ * Supply your own confirmation implementations to this function to create a custom confirmation
+ * strategy for recently-landed transactions.
+ *
+ * @example
+ * ```ts
+ * import { waitForRecentTransactionConfirmation } from '@solana/transaction-confirmation';
+ *
+ * try {
+ *     await waitForRecentTransactionConfirmation({
+ *         getBlockHeightExceedencePromise({ abortSignal, commitment, lastValidBlockHeight }) {
+ *             // Return a promise that rejects when the blockhash's block height has been exceeded
+ *         },
+ *         getRecentSignatureConfirmationPromise({ abortSignal, commitment, signature }) {
+ *             // Return a promise that resolves when a transaction achieves confirmation
+ *         },
+ *     });
+ * } catch (e) {
+ *     // Handle errors.
+ * }
+ * ```
+ */
 export async function waitForRecentTransactionConfirmation(
     config: WaitForRecentTransactionWithBlockhashLifetimeConfirmationConfig,
 ): Promise<void> {
