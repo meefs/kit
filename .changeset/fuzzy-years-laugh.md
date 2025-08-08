018479f56dc7f487b9a9ec444184cea7f13d9f3a
Loris Leiva <loris.leiva@gmail.com>
2025-08-08 14:49:27 +0100

    Export more helpers from instruction plans package (#727)
    This PR exports all types and functions from the `@solana/instruction-plans` package. Currently it only exports the `InstructionPlan` factory helpers. It was a mistake on my side not to update the `index.ts` file as I was building the package.

---
+++ .changeset/fuzzy-years-laugh.md
@@ -0,0 +1,5 @@
+---
+'@solana/instruction-plans': patch
+---
+
+Export more types and functions from the `@solana/instruction-plans` package.
--- packages/instruction-plans/src/index.ts
+++ packages/instruction-plans/src/index.ts
@@ -4,3 +4,7 @@
  * @packageDocumentation
  */
 export * from './instruction-plan';
+export * from './transaction-plan-executor';
+export * from './transaction-plan-result';
+export * from './transaction-plan';
+export * from './transaction-planner';
