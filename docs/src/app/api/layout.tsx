e3a468ffd494b708f8b71b69196472f1531522ec
Loris Leiva <loris.leiva@gmail.com>
2025-08-19 09:49:31 +0100

    Use same sidebar on API and docs layouts (#744)
    This PR fixes the API layout of the documentation such that it matches the ones used by the "Docs" layout — that is, the part of the documentation that contains guides.
    
    In other words, it ensures the sidebar and navbar are the same regardless of whether we are in the "Documentation" area or the "API References" area.

--- docs/src/app/api/layout.tsx
+++ docs/src/app/api/layout.tsx
@@ -1,19 +1,21 @@
-import { DocsLayout } from 'fumadocs-ui/layouts/docs';
+import { DocsLayout, DocsLayoutProps } from 'fumadocs-ui/layouts/docs';
 import type { ReactNode } from 'react';
 import { baseOptions } from '@/app/layout.config';
 import { apiSource } from '@/lib/source';
+import { Sidebar } from '../docs/sidebar';
+import { Navbar } from '../docs/navbar';
 
 export default function Layout({ children }: { children: ReactNode }) {
-    return (
-        <DocsLayout
-            // tree={{
-            //     ...apiSource.pageTree,
-            //     children: apiSource.pageTree.children.filter(node => node.type !== 'page' || node.url !== '/api'),
-            // }}
-            tree={apiSource.pageTree}
-            {...baseOptions}
-        >
-            {children}
-        </DocsLayout>
-    );
+    const baseProps: DocsLayoutProps = {
+        ...baseOptions,
+        tree: apiSource.pageTree,
+    };
+
+    const apiProps: DocsLayoutProps = {
+        ...baseProps,
+        sidebar: { ...baseProps.sidebar, component: Sidebar(baseProps) },
+        nav: { ...baseProps.nav, component: Navbar(baseProps) },
+    };
+
+    return <DocsLayout {...apiProps}>{children}</DocsLayout>;
 }
