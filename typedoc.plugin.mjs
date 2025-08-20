3c783ca953cd6f8d35e11f781eeb299daa30dd55
Steven Luscher <steveluscher@users.noreply.github.com>
2025-08-20 03:23:52 -0400

    Format method signatures as code blocks in API docs (#752)
    #### Problem
    
    These are super ugly.
    
    #### Summary of Changes
    
    - Enabled `useCodeBlocks`
    - Ran prettier over the signatures that get produced
    
    | Before | After |
    | --- | --- |
    | ![image.png](https://app.graphite.dev/user-attachments/assets/df81ae62-c2e2-4231-ae1d-ec6cb4b940d9.png) | ![image.png](https://app.graphite.dev/user-attachments/assets/628542f6-34d9-4fb7-ac14-faec9d848999.png) |

--- typedoc.plugin.mjs
+++ typedoc.plugin.mjs
@@ -1,9 +1,67 @@
 // @ts-check
-import { dirname, resolve } from 'node:path';
+import prettier from '@prettier/sync';
+import { existsSync } from 'node:fs';
+import { dirname, join, resolve } from 'node:path';
 import * as td from 'typedoc-plugin-markdown';
+
+// Fits on a 13" MBP screen (1280px width) given the current design
+const METHOD_SIGNATURE_COLUMN_WIDTH = 72;
+
+/** @type {import("prettier").Options | null | undefined} */
+let prettierConfig;
+function getPrettierConfig() {
+    if (prettierConfig === undefined) {
+        const PATH = join(import.meta.dirname, 'docs', 'content', '.prettierrc');
+        if (existsSync(PATH)) {
+            prettierConfig = prettier.resolveConfig(PATH);
+        } else {
+            prettierConfig = null;
+            console.warn(
+                `No Pretter config file exists at ${PATH}; Method signatures in generated API ` +
+                    'documentation may not be formatted according to the same options as the ' +
+                    'rest of the docs',
+            );
+        }
+    }
+    return prettierConfig;
+}
+
+class KitDocsMarkdownTheme extends td.MarkdownTheme {
+    /** @param {td.MarkdownPageEvent<import('typedoc').Reflection>} page */
+    getRenderContext(page) {
+        return new KitDocsThemeRenderContext(this, page, this.application.options);
+    }
+}
+
+class KitDocsThemeRenderContext extends td.MarkdownThemeContext {
+    /** @param {ConstructorParameters<typeof td.MarkdownThemeContext>} args */
+    constructor(...args) {
+        super(...args);
+        const oldSignatureTitle = this.partials.signatureTitle;
+        this.partials = {
+            ...this.partials,
+            signatureTitle(model, options) {
+                const titleMarkdown = oldSignatureTitle(model, options);
+                const prettierConfig = getPrettierConfig();
+                const prettyTitleMarkdown = prettier.format(titleMarkdown, {
+                    ...prettierConfig,
+                    printWidth: Math.min(
+                        prettierConfig?.printWidth ?? METHOD_SIGNATURE_COLUMN_WIDTH,
+                        METHOD_SIGNATURE_COLUMN_WIDTH,
+                    ),
+                    parser: 'markdown',
+                });
+                return prettyTitleMarkdown;
+            },
+        };
+    }
+}
 
 /** @param {td.MarkdownApplication} app */
 export function load(app) {
+    // Use this theme name in typedoc.json or when using the CLI
+    app.renderer.defineTheme('kit-docs-theme', KitDocsMarkdownTheme);
+
     // Set Markdown frontmatter for each page
     app.renderer.on(td.MarkdownPageEvent.BEGIN, page => {
         page.frontmatter = {
